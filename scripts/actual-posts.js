a447dd4df3933177c729d71c841e0be130b45825
John Micheal <kedeesjohn200@gmail.com>
2024-12-04 18:40:35 +0200

    placed the posts and stories into a function

--- scripts/actual-posts.js
+++ scripts/actual-posts.js
@@ -1,140 +1,153 @@
-
-
-const postsHTML = `
-          <div class="actual-post">
+function newPostHTML(profileImage, profileName, postText, postImage) {
+    const postHTML = `
+        <div class="actual-post">
             <div class="content-head">
-              <img src="images/profile-icon.jpeg" class="profile-icon">
-              <span>Markiplier </span>
+                <img src="${profileImage}" class="profile-icon">
+                <span>${profileName} </span>
             </div>
             <div class="content-text">
-              <span>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic, sed cum porro blanditiis amet magnam sunt eligendi aperiam nihil illo cupiditate earum ratione, est aliquam! Eos accusantium labore veritatis ducimus!
-              </span>
-              <div class="image-post-container">
-                <img src="images/post-image-test.jpg" class="post-image">
-              </div>
+                <span>
+                    ${postText}
+                </span>
+                <div class="image-post-container">
+                    <img src="${postImage}" class="post-image">
+                </div>
             </div>
             <div class="post-footer">
-              <div class="reacts">
-                <div>
-                  &#128514;
-                  <span>20k</span>
+                <div class="reacts">
+                    <div>
+                        &#128514;
+                        <span>20k</span>
+                    </div>
+                    <div>
+                        <span>1.8k comments</span>
+                        <span>2.5k Shares</span>
+                    </div>
                 </div>
-                <div>
-                  <span>1.8k comments</span>
-                  <span>2.5k Shares</span>
+                <div class="react-types">
+                    <div class="react-icons">
+                        <img src="images/another/like.png" alt="">
+                        <span>Like</span>
+                    </div>
+                    <div class="react-icons">
+                        <img src="images/another/comment.png" alt="">
+                        <span>Comment</span>
+                    </div>
+                    <div class="react-icons">
+                        <img src="images/another/send.png" alt="">
+                        <span>Send</span>
+                    </div>
+                    <div class="react-icons">
+                        <img src="images/another/share.png" alt="">
+                        <span>Share</span>
+                    </div>
                 </div>
-              </div>
-              <div class="react-types">
-                <div class="react-icons">
-                  <img src="images/another/like.png" alt="">
-                  <span>Like</span>
-                </div>
-                <div class="react-icons">
-                  <img src="images/another/comment.png" alt="">
-                  <span>Comment</span>
-                </div>
-                <div class="react-icons">
-                  <img src="images/another/send.png" alt="">
-                  <span>Send</span>
-                </div>
-                <div class="react-icons">
-                  <img src="images/another/share.png" alt="">
-                  <span>Share</span>
-                </div>
-              </div>
             </div>
-          </div>
-          <div class="actual-post">
-            <div class="content-head">
-              <img src="images/profile-icon.jpeg" class="profile-icon">
-              <span>Markiplier </span>
-            </div>
-            <div class="content-text">
-              <span>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic, sed cum porro blanditiis amet magnam sunt eligendi aperiam nihil illo cupiditate earum ratione, est aliquam! Eos accusantium labore veritatis ducimus!
-              </span>
-              <div class="image-post-container">
-                <img src="images/another-meme.jpg" class="post-image">
-              </div>
-            </div>
-            <div class="post-footer">
-              <div class="reacts">
-                <div>
-                  &#128514;
-                  <span>20k</span>
-                </div>
-                <div>
-                  <span>1.8k comments</span>
-                  <span>2.5k Shares</span>
-                </div>
-              </div>
-              <div class="react-types">
-                <div class="react-icons">
-                  <img src="images/another/like.png" alt="">
-                  <span>Like</span>
-                </div>
-                <div class="react-icons">
-                  <img src="images/another/comment.png" alt="">
-                  <span>Comment</span>
-                </div>
-                <div class="react-icons">
-                  <img src="images/another/send.png" alt="">
-                  <span>Send</span>
-                </div>
-                <div class="react-icons">
-                  <img src="images/another/share.png" alt="">
-                  <span>Share</span>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="actual-post">
-            <div class="content-head">
-              <img src="images/profile-icon.jpeg" class="profile-icon">
-              <span>Markiplier </span>
-            </div>
-            <div class="content-text">
-              <span>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic, sed cum porro blanditiis amet magnam sunt eligendi aperiam nihil illo cupiditate earum ratione, est aliquam! Eos accusantium labore veritatis ducimus!
-              </span>
-              <div class="image-post-container">
-                <img src="images/tot-tot.jpg" class="post-image">
-              </div>
-            </div>
-            <div class="post-footer">
-              <div class="reacts">
-                <div>
-                  &#128514;
-                  <span>20k</span>
-                </div>
-                <div>
-                  <span>1.8k comments</span>
-                  <span>2.5k Shares</span>
-                </div>
-              </div>
-              <div class="react-types">
-                <div class="react-icons">
-                  <img src="images/another/like.png" alt="">
-                  <span>Like</span>
-                </div>
-                <div class="react-icons">
-                  <img src="images/another/comment.png" alt="">
-                  <span>Comment</span>
-                </div>
-                <div class="react-icons">
-                  <img src="images/another/send.png" alt="">
-                  <span>Send</span>
-                </div>
-                <div class="react-icons">
-                  <img src="images/another/share.png" alt="">
-                  <span>Share</span>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+        </div> 
+    `;
+    const postsContainer = document.querySelector(".js-posts-container");
+    postsContainer.innerHTML = postHTML + postsContainer.innerHTML;
+}
 
-`;
+newPostHTML(
+    "images/profile-icon.jpeg",
+    "John Micheal",
+    "This is a new post!",
+    "images/tot-tot.jpg"
+);
 
-document.querySelector('.js-posts-container').innerHTML = postsHTML;
+newPostHTML(
+    "images/profile-icon.jpeg",
+    "Peter Riad",
+    "This is a new post!",
+    "images/tot-tot.jpg"
+);
+
+newPostHTML(
+    "images/profile-icon.jpeg",
+    "Joe Suliman",
+    "This is a new post!",
+    "images/tot-tot.jpg"
+);
+newPostHTML(
+  "images/profile-icon.jpeg",
+  "John Micheal",
+  "This is a new post!",
+  "images/tot-tot.jpg"
+);
+
+newPostHTML(
+  "images/profile-icon.jpeg",
+  "Peter Riad",
+  "This is a new post!",
+  "images/tot-tot.jpg"
+);
+
+newPostHTML(
+  "images/profile-icon.jpeg",
+  "Joe Suliman",
+  "This is a new post!",
+  "images/tot-tot.jpg"
+);
+
+document.querySelector(".js-posts-container").innerHTML = postsContainer;
+
+
+
+
+
+// function createNewPost(profileImage, username, postText, postImage) {
+//     const newPostHTML = `
+//         <div class="actual-post">
+//             <div class="content-head">
+//                 <img src="${profileImage}" class="profile-icon">
+//                 <span>${username}</span>
+//             </div>
+//             <div class="content-text">
+//                 <span>${postText}</span>
+//                 <div class="image-post-container">
+//                     <img src="${postImage}" class="post-image">
+//                 </div>
+//             </div>
+//             <div class="post-footer">
+//                 <div class="reacts">
+//                     <div>
+//                         &#128514;
+//                         <span>0</span>
+//                     </div>
+//                     <div>
+//                         <span>0 comments</span>
+//                         <span>0 Shares</span>
+//                     </div>
+//                 </div>
+//                 <div class="react-types">
+//                     <div class="react-icons">
+//                         <img src="images/another/like.png" alt="">
+//                         <span>Like</span>
+//                     </div>
+//                     <div class="react-icons">
+//                         <img src="images/another/comment.png" alt="">
+//                         <span>Comment</span>
+//                     </div>
+//                     <div class="react-icons">
+//                         <img src="images/another/send.png" alt="">
+//                         <span>Send</span>
+//                     </div>
+//                     <div class="react-icons">
+//                         <img src="images/another/share.png" alt="">
+//                         <span>Share</span>
+//                     </div>
+//                 </div>
+//             </div>
+//         </div>
+//     `;
+//     // Prepend the new post to the posts container
+//     const postsContainer = document.querySelector('.js-posts-container');
+//     postsContainer.innerHTML = newPostHTML + postsContainer.innerHTML;
+// }
+// // Example usage
+// createNewPost('images/profile-icon.jpeg', 'New User', 'This is a new post!', 'images/new-post-image.jpg');
+// createNewPost('images/profile-icon.jpeg', 'New User', 'This is a new post!', 'images/new-post-image.jpg');
+// createNewPost('images/profile-icon.jpeg', 'New User', 'This is a new post!', 'images/new-post-image.jpg');
+
+// document.querySelector('.js-posts-container').innerHTML = postsContainer;
--- scripts/stories.js
+++ scripts/stories.js
@@ -1,101 +1,52 @@
+function newStories(storyImage, storyName) {
+  const storyHTML = `
+    <div class="stories swiper-slide">
+      <div class="story-contain-image">
+        <img src="${storyImage}" class="stories-icons">
+      </div>
+      <span class="stories-name">${storyName}</span>
+    </div>
+  `;
+  const storyWrapper = document.querySelector('.js-stories-container .swiper-wrapper');
+  storyWrapper.innerHTML += storyHTML;
+}
 
- const storiesHTML = `
-          <button class="arrow-back material-symbols-outlined">
-            arrow_back_ios
-          </button>
-          <div class="swiper-wrapper">
-            <div class="stories swiper-slide">
-              <div class="story-contain-image">
-                <img src="images/Admiral-Custom-Pet-Portrait-7.jpg" class="stories-icons">
-              </div>
-              <span class="stories-name">Dog Admiral</span>
-            </div>
-            <div class="stories swiper-slide">
-              <div class="story-contain-image">
-                <img src="images/Admiral-cat.jpg" class="stories-icons">
-              </div>
-              <span class="stories-name">Cat Admiral</span>
-            </div>
-            <div class="stories swiper-slide">
-              <div class="story-contain-image">
-                <img src="images/prince-cat.webp" class="stories-icons">
-              </div>
-              <span class="stories-name">Cat Prince</span>
-            </div>
-            <div class="stories swiper-slide">
-              <div class="story-contain-image">
-                <img src="images/Cat lord.jpg" class="stories-icons">
-              </div>
-              <span class="stories-name">Cat Lord</span>
-            </div>
-            <div class="stories swiper-slide">
-              <div class="story-contain-image">
-                <img src="images/Admiral-Custom-Pet-Portrait-7.jpg" class="stories-icons">
-              </div>
-              <span class="stories-name">Dog Admiral</span>
-            </div>
-            <div class="stories swiper-slide">
-              <div class="story-contain-image">
-                <img src="images/Admiral-Custom-Pet-Portrait-7.jpg" class="stories-icons">
-              </div>
-              <span class="stories-name">Dog Admiral</span>
-            </div>
-          </div>
-          <button class="arrow-forward material-symbols-outlined">
-            arrow_forward_ios
-          </button>
- `;
- document.querySelector('.js-stories-container').innerHTML = storiesHTML;
 
-  document.addEventListener("DOMContentLoaded", () => {
-    const swiper = new Swiper('.js-stories-container', {
-      slidesPerView: 'auto',
-      spaceBetween: 10,
-      centeredSlides: true,
-      loop: true,
-      loopAdditionalSlides: 1,  // Ensures enough slides are duplicated
-      navigation: {
-        nextEl: '.arrow-forward',
-        prevEl: '.arrow-back',
-      },
-      breakpoints: {
-        // When window width is >= 320px
-        320: {
-          slidesPerView: 'auto',
-          spaceBetween: 2
-        },
-        // When window width is >= 480px
-        480: {
-          slidesPerView: 'auto',
-          spaceBetween: 2
+document.addEventListener("DOMContentLoaded", () => {
+  // Add stories
+  newStories("images/Admiral-cat.jpg", "First Admiral");
+  newStories("images/Admiral-cat.jpg", "Second Admiral");
+  newStories("images/Admiral-cat.jpg", "Third Admiral");
+  newStories("images/Admiral-cat.jpg", "Third Admiral");
+  newStories("images/Admiral-cat.jpg", "Third Admiral");
+  newStories("images/Admiral-cat.jpg", "Third Admiral");
 
-        },
-        // When window width is >= 640px
-        640: {
-          slidesPerView: 'auto',
-          spaceBetween: 2
-        },
-        // When window width is >= 768px
-        768: {
-          slidesPerView: 'auto',
-          spaceBetween: 10
-        }
-      }
-      
-    });
-
-    // Function to update Swiper on window resize
-    function updateSwiper() {
-      if (window.innerWidth <= 480) {
-        swiper.params.centeredSlides = true;
-      } else {
-        swiper.params.centeredSlides = false;
-      }
-      swiper.update();
-    }
-
-    // Initial call and event listener for window resize
-    updateSwiper();
-    window.addEventListener('resize', updateSwiper);
+  // Initialize Swiper
+  const swiper = new Swiper('.js-stories-container', {
+    slidesPerView: 'auto',
+    spaceBetween: 10,
+    centeredSlides: true,
+    loop: true,
+    loopAdditionalSlides: 1,
+    navigation: {
+      nextEl: '.arrow-forward',
+      prevEl: '.arrow-back',
+    },
+    breakpoints: {
+      320: { slidesPerView: 'auto', spaceBetween: 2 },
+      480: { slidesPerView: 'auto', spaceBetween: 2 },
+      640: { slidesPerView: 'auto', spaceBetween: 2 },
+      768: { slidesPerView: 'auto', spaceBetween: 10 },
+    },
   });
+
+  // Update Swiper on resize
+  function updateSwiper() {
+    swiper.params.centeredSlides = window.innerWidth <= 480;
+    swiper.update();
+  }
+  updateSwiper();
+  window.addEventListener('resize', updateSwiper);
+});
+
 
